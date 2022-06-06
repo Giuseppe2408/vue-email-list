dc5eba104eac6f27c02443436a1d8a2100721671
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-06-06 17:43:57 +0200

    fine

--- data.js
+++ data.js
@@ -6,12 +6,21 @@ const aPI = new Vue({
     },
     
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-            const resultMail = result.data.response;
-            console.log(resultMail);
-            this.email = resultMail;
-        })
+        
+            for (let index = 0; index < 10; index++) {   
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                const resultMail = result.data.response;
+                console.log(resultMail);            
+                this.email.push(resultMail); 
+            })
+            
+
+        }
+        
     },
+
+
+
 
 
 
